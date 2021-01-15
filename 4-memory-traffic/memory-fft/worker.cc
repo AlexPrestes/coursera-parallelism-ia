cdcf2f3de44e13ca76b9d0cb3f56c39398fb8d27
Alex Prestes <alex.prestes@usp.br>
2021-01-15 19:37:39 -0300

    semana 4 hbm

--- 4-memory-traffic/memory-fft/worker.cc
+++ 4-memory-traffic/memory-fft/worker.cc
@@ -1,9 +1,30 @@
+#include "hbwmalloc.h"
 #include <mkl.h>
-
 
 //implement scratch buffer on HBM and compute FFTs, refer instructions on Lab page
 void runFFTs( const size_t fft_size, const size_t num_fft, MKL_Complex8 *data, DFTI_DESCRIPTOR_HANDLE *fftHandle) {
-  for(size_t i = 0; i < num_fft; i++) {
-    DftiComputeForward (*fftHandle, &data[i*fft_size]);
+  
+  MKL_Complex8* buff;
+
+  hbw_posix_memalign((void**) &buff, 4096, sizeof(MKL_Complex8)*fft_size);
+
+  for(size_t j = 0; j < num_fft; j++) {
+    //DftiComputeForward (*fftHandle, &data[i*fft_size]);
+
+    #pragma omp parallel for
+    for(size_t i = 0; i < fft_size; i++) {
+  	  buff[i].real = data[i + j*fft_size].real;
+  	  buff[i].imag = data[i + j*fft_size].imag;
+    }
+
+    DftiComputeForward(*fftHandle, &buff[0]);
+    
+    #pragma omp parallel for
+    for(size_t i=0; i < fft_size; i++) {
+      data[i + j*fft_size].real = buff[i].real;
+      data[i + j*fft_size].imag = buff[i].imag;
+    }
   }
+
+  hbw_free(buff);
 }
